8137b7a1bb7abbb313b58ebc1c9e5ec5da4148d5
Yoann <contact@yoannvie.fr>
2017-07-11 17:47:40 +0200

    #all : fix warnings and global errors

--- Projet.c
+++ Projet.c
@@ -4,48 +4,7 @@
 #include <time.h>
 
 
-void menu(int i){
-    char j[20];
-    while( i != 0){
-        printf("Bienvenue dans la Banque Picsou \nChoisissez votre action:\n1 - Gestion des Clients \n2 - Gestion des comptes \n3 - Gestion des operations \n4 - Administration \n0 - Partir de la Banque\n");
-        scanf("%s",&j);
-        i = atoi(j);
-        if ( i == 1){
-            gestion_client(i);
-        }
-        else if ( i == 2){
-            gestion_compte(i);
-        }
-        else if ( i == 3){
-            gestion_operation(i);
-        }
-        else if ( i == 4){
-            administration(i);
-        }
-    }
-    printf("Vous quittez la banque Picsou !");
-}
 
-void gestion_client(int i){
-    char j[20];
-    while( i != 0){
-        printf("Gestion de clients \nChoisissez votre action: \n1 - Ajouter un client\n2 - Modifier un client \n3 - Supprimer un client \n4 - Rechercher un client \n0 - Retourner au menu principal\n");
-        scanf("%s",&j);
-        i = atoi(j);
-        if ( i == 1){
-            ajouter_client();
-        }
-        else if ( i == 2){
-            modifier_client();
-        }
-        else if ( i == 3){
-            supprimer_client();
-        }
-        else if ( i == 4){
-            rechercher_client();
-        }
-    }
-}
 
 void ajouter_client(){
     srand(time(NULL));
@@ -55,13 +14,13 @@ void ajouter_client(){
     char profession[50];
     char telephone[13];
     printf("Veuillez entrez les informations concernant le client:\nLe nom : ");
-    scanf("%s",&nom);
+    scanf("%s",nom);
     printf("Le Prenom : ");
-    scanf("%s",&prenom);
+    scanf("%s",prenom);
     printf("La profession : ");
-    scanf("%s",&profession);
+    scanf("%s",profession);
     printf("Le telephone : ");
-    scanf("%s",&telephone);
+    scanf("%s",telephone);
 
     //Inscription du client dans le fichier CSV
 
@@ -87,16 +46,16 @@ void modifier_client(){
     printf("Veuillez entrez l'id du client a modifier : ");
     scanf("%d",&id);
 
-    // Sélection du clients dans le fichier CSV
+    // Sï¿½lection du clients dans le fichier CSV
 
     printf("Veuillez entrez maintenant les  nouvelles informations concernant le client:\nSon nom : ");
-    scanf("%s",&nom);
+    scanf("%s",nom);
     printf("Son Prenom : ");
-    scanf("%s",&prenom);
+    scanf("%s",prenom);
     printf("Sa profession : ");
-    scanf("%s",&profession);
+    scanf("%s",profession);
     printf("Son telephone : ");
-    scanf("%s",&telephone);
+    scanf("%s",telephone);
 
     // Modification du client dans le CSV
 
@@ -114,28 +73,10 @@ void supprimer_client(){
 void rechercher_client(){
     char nom[50];
     printf("Veuillez entrez le nom du client a rechercher : ");
-    scanf("%s",&nom);
+    scanf("%s",nom);
 
     // Recherche du client dans le CSV
 
-}
-
-void gestion_compte(int i){
-    char j[20];
-    while( i != 0){
-        printf("Gestion de comptes \nChoisissez votre action: \n1 - Creer un nouveau compte \n2 - consulter un compte \n3 - Fermer un compte \n0 - Retourner au menu principal\n");
-        scanf("%s",&j);
-        i = atoi(j);
-        if ( i == 1){
-            creer_compte();
-        }
-        else if ( i == 2){
-            consulter_compte();
-        }
-        else if ( i == 3){
-            fermer_compte();
-        }
-    }
 }
 
 void creer_compte(){
@@ -147,7 +88,7 @@ void creer_compte(){
     int duree;
 
     printf("Veuillez entrez les informations concernant le Compte:\nLe nom  du proprietaire: ");
-    scanf("%s",&proprio);
+    scanf("%s",proprio);
     printf("Le solde : ");
     scanf("%f",&solde);
     printf("Le taux : ");
@@ -163,7 +104,7 @@ void creer_compte(){
 void consulter_compte(){
     int proprio;
     printf("Veuillez entrez le nom du proprietaire du compte a rechercher : ");
-    scanf("%s",&proprio);
+    scanf("%s",proprio);
 
     // Recherche du compte dans le CSV
 
@@ -178,20 +119,20 @@ void fermer_compte(){
 
 }
 
-void gestion_operation(int i){
+void gestion_compte(int i){
     char j[20];
     while( i != 0){
-        printf("Gestion des opérations \nChoisissez votre action: \n1 - Depot \n2 - Retrait \n3 - Virement \n0 - Retourner au menu principal\n");
-        scanf("%s",&j);
+        printf("Gestion de comptes \nChoisissez votre action: \n1 - Creer un nouveau compte \n2 - consulter un compte \n3 - Fermer un compte \n0 - Retourner au menu principal\n");
+        scanf("%s",j);
         i = atoi(j);
         if ( i == 1){
-            deposer_argent();
+            creer_compte();
         }
         else if ( i == 2){
-            retirer_argent();
+            consulter_compte();
         }
         else if ( i == 3){
-            virer_argent();
+            fermer_compte();
         }
     }
 }
@@ -232,11 +173,49 @@ void virer_argent(){
     // modification du compte dans le CSV
 }
 
+void gestion_operation(int i){
+    char j[20];
+    while( i != 0){
+        printf("Gestion des opï¿½rations \nChoisissez votre action: \n1 - Depot \n2 - Retrait \n3 - Virement \n0 - Retourner au menu principal\n");
+        scanf("%s",j);
+        i = atoi(j);
+        if ( i == 1){
+            deposer_argent();
+        }
+        else if ( i == 2){
+            retirer_argent();
+        }
+        else if ( i == 3){
+            virer_argent();
+        }
+    }
+}
+
+void afficher_somme_all(){
+    //la somme des montants de tous les comptes du CSV
+}
+
+void afficher_somme_type(){
+    //la somme des montants par type de compte du CSV
+}
+
+void afficher_montant_interets(){
+    //le montant dï¿½intï¿½rï¿½ts total et par type de compte que devra verser la banque a la fin de lï¿½annï¿½e du CSV
+}
+
+void exporter(){
+    //Exporter lï¿½ensemble des donnï¿½es de lï¿½application
+}
+
+void importer(){
+    //Importer des donnï¿½es dans lï¿½application (selon un format que vous dï¿½finirez, il est fortement conseillï¿½ de se baser sur le format qui vous sert a exporter les donnï¿½es). Dans le cas oï¿½ lï¿½application contient dï¿½jï¿½ des donnï¿½es, les donnï¿½es importï¿½es devront fusionner avec les anciennes.
+}
+
 void administration(int i){
     char j[20];
     while( i != 0){
         printf("Administration \nChoisissez votre action: \n1 - Afficher la somme des montants de tous les comptes \n2 - Afficher la somme des montants par type de compte \n3 - Afficher le montant d'interets total et par type de compte que devra verser la banque a la fin de l'annee \n4 - Exporter l'ensemble des donnees de l'application \n5 - Importer des donnees dans l'application \n0 - Retourner au menu principal\n");
-        scanf("%s",&j);
+        scanf("%s",j);
         i = atoi(j);
         if ( i == 1){
             afficher_somme_all();
@@ -256,29 +235,52 @@ void administration(int i){
     }
 }
 
-void afficher_somme_all(){
-    //la somme des montants de tous les comptes du CSV
+void gestion_client(int i){
+    char j[20];
+    while( i != 0){
+        printf("Gestion de clients \nChoisissez votre action: \n1 - Ajouter un client\n2 - Modifier un client \n3 - Supprimer un client \n4 - Rechercher un client \n0 - Retourner au menu principal\n");
+        scanf("%s",j);
+        i = atoi(j);
+        if ( i == 1){
+            ajouter_client();
+        }
+        else if ( i == 2){
+            modifier_client();
+        }
+        else if ( i == 3){
+            supprimer_client();
+        }
+        else if ( i == 4){
+            rechercher_client();
+        }
+    }
 }
 
-void afficher_somme_type(){
-    //la somme des montants par type de compte du CSV
+void menu(int i){
+    char j[20];
+    while( i != 0){
+        printf("Bienvenue dans la Banque Picsou \nChoisissez votre action:\n1 - Gestion des Clients \n2 - Gestion des comptes \n3 - Gestion des operations \n4 - Administration \n0 - Partir de la Banque\n");
+        scanf("%s", j);
+        i = atoi(j);
+        if ( i == 1){
+            gestion_client(i);
+        }
+        else if ( i == 2){
+            gestion_compte(i);
+        }
+        else if ( i == 3){
+            gestion_operation(i);
+        }
+        else if ( i == 4){
+            administration(i);
+        }
+    }
+    printf("Vous quittez la banque Picsou !");
 }
-
-void afficher_montant_interets(){
-    //le montant d’intérêts total et par type de compte que devra verser la banque a la fin de l’année du CSV
-}
-
-void exporter(){
-    //Exporter l’ensemble des données de l’application
-}
-
-void importer(){
-    //Importer des données dans l’application (selon un format que vous définirez, il est fortement conseillé de se baser sur le format qui vous sert a exporter les données). Dans le cas où l’application contient déjà des données, les données importées devront fusionner avec les anciennes.
-}
-
 
 int main(void){
     menu(1);
+
     return 0;
 }
 
